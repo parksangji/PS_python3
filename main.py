a0b3bf2fd91d2f4253aca8b944d5a7a57f8cadd3
SangJi <59435705+parksangji@users.noreply.github.com>
2022-04-11 06:27:15 +0000

    boj

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import sys
-# 1051 다이나믹 프로그래밍 + 브루트포스
 input = sys.stdin.readline
+
+n,jimin,hansu = map(int,input().split())
+cnt = 0
+print(-jimin^-hansu)
+while jimin != hansu :
+  jimin -= jimin//2
+  hansu -= hansu//2
+  cnt += 1
+else :
+  print(cnt)
