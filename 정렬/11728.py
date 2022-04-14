70892d7b2c3d71cf01c6b8237f4f34fc2210a583
SangJi <59435705+parksangji@users.noreply.github.com>
2022-04-14 08:02:30 +0000

    boj 정렬

--- 정렬/11728.py
+++ 정렬/11728.py
@@ -1,7 +1,3 @@
 import sys
-
-a,b = map(int,sys.stdin.readline().split())
-arr = list(map(int,sys.stdin.readline().split()))
-arr.extend(list(map(int,sys.stdin.readline().split())))
-for i in sorted(arr) :
-  print(i,end = " ")
+input()
+print(' '.join(sorted(sys.stdin.read().split(), key=int)))
