de5765c2a09ce19eb0627533cabf16a075bad4e6
SangJi <59435705+parksangji@users.noreply.github.com>
2022-04-18 06:03:01 +0000

    boj

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import sys; n = int(sys.stdin.readline())
-
-dp = [[0,[]] for _ in range(n+1)]
-dp[1][1] = [1]
-for i in range(2,n+1) :
-  dp[i][0] = dp[i-1][0] + 1
-  dp[i][1] = dp[i-1][1] + [i]
-  if i % 2 == 0 and dp[i//2][0] + 1 < dp[i][0]:
-    dp[i][0] = dp[i//2][0] + 1
-    dp[i][1] = dp[i//2][1] + [i]
-  if i % 3 == 0 and dp[i//3][0] + 1 < dp[i][0]:
-    dp[i][0] = dp[i//3][0] + 1
-    dp[i][1] = dp[i//3][1] + [i]
-    
-print(dp[n][0])
-for i in range(len(dp[n][1]) -1 ,-1,-1) :
-  print(dp[n][1][i], end = " ")
+arr = list(map(int,sys.stdin.readline().split()))
+start = 0; end = n-1
+compare = int(sys.maxsize)
+answer = []
+while start < end :
+  sum = arr[start] + arr[end]
+  if abs(sum) < abs(compare) :
+    compare = sum
+    answer = [arr[start],arr[end]]
+  if 0 <= sum :
+    end -= 1
+  else :
+    start += 1
+print(answer[0],answer[1])
