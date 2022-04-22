c06ad7888a4d506d9099d3e027123ea04c22eccf
SangJi <59435705+parksangji@users.noreply.github.com>
2022-04-22 13:29:31 +0000

    boj

--- main.py
+++ main.py
@@ -1,47 +1,11 @@
-import sys; from collections import deque ;input = sys.stdin.readline
-def init() :
-  coin = []
-  for i in range(n) :
-    for j in range(m) :
-      if arr[i][j] == 'o' :
-        coin.append([i,j])
-  return coin[0],coin[1]
-
-def outRange(x,y) :
-  if 0 <= x < n and 0 <= y < m :
-    return False
-  return True
-
-def bfs(a,b,c,d) :
-  q = deque()
-  visited = {}
-  q.append([a,b,c,d,0])
-  visited[str(a) + str(b) + str(c) + str(d)] = True
-  while q :
-    a,b,c,d,cnt = q.popleft()
-    if cnt >= 10 :
-      continue
-    for i in range(4) :
-      aa,bb = a + dx[i],b + dy[i]
-      cc,dd = c + dx[i],d + dy[i]
-      if outRange(aa,bb) and outRange(cc,dd) :
-        continue
-      if outRange(aa,bb) or outRange(cc,dd) :
-        return cnt+1
-      if arr[aa][bb] == "#" :
-        aa -= dx[i]
-        bb -= dy[i]
-      if arr[cc][dd] == "#" :
-        cc -= dx[i]
-        dd -= dy[i]
-      flag = str(aa) + str(bb) + str(cc) + str(dd)
-      if flag in visited :
-        continue
-      visited[flag] = True
-      q.append([aa,bb,cc,dd,cnt+1])
-  return -1
-dx = [0,0,-1,1] ; dy = [-1,1,0,0]
-n,m = map(int,input().split())
-arr = [list(input().strip()) for _ in range(n)]
-coin1,coin2 = init()
-print(bfs(coin1[0],coin1[1],coin2[0],coin2[1]))
+import sys;
+def zeroone(n) :
+  if n== 0 :
+    return 0 
+  if n == 1 :
+    return 1
+  if n%2 :
+    return 1 - zeroone(n//2)
+  else :
+    return zeroone(n//2)
+print(zeroone(int(sys.stdin.readline())-1))
